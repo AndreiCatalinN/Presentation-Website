e673d4358f71dd8d1e4be30b5e13429fb6d867b1
AndreiCatalinN <negura.a.catalin@gmail.com>
2025-08-15 19:49:32 +0300

    added toast

--- service-worker.js
+++ service-worker.js
@@ -9,12 +9,18 @@ const STATIC_ASSETS = [
   '/service-worker.js',
 ];
 
-// Install event – cache essential files
+function sendMessageToClient(clientId, msg) {
+  clients.get(clientId).then(client => {
+    if (client) client.postMessage(msg);
+  });
+}
+
+
 self.addEventListener('install', (event) => {
-  console.log('[ServiceWorker] Install');
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching assets');
+
       return cache.addAll(STATIC_ASSETS);
     })
   );
@@ -22,13 +28,13 @@ self.addEventListener('install', (event) => {
 
 // Activate event – clean old caches
 self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
+  
   event.waitUntil(
     caches.keys().then((cacheNames) =>
       Promise.all(
         cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache:', cache);
+            
             return caches.delete(cache);
           }
         })
@@ -40,14 +46,18 @@ self.addEventListener('activate', (event) => {
 
 
 self.addEventListener('fetch', (event) => {
-  console.log('[ServiceWorker] Fetching:', event.request.url);
+  
 
   if (event.request.method !== 'GET') return; 
 
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
       if (cachedResponse) {
-        console.log('[ServiceWorker] Serving from cache:', event.request.url);
+        sendMessageToClient(event.clientId, {
+          type: 'CACHE_HIT',
+          url: event.request.url
+        });
+        
         return cachedResponse;
       }
 
